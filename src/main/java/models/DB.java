46d627fae1eac499f7423ddb87821465198fecb8
MichaelOdera <michogelira@gmail.com>
2021-04-15 22:42:33 +0300

    added changes for the ssl requirement of the database

--- src/main/java/models/DB.java
+++ src/main/java/models/DB.java
@@ -6,5 +6,5 @@ public class DB {
     //public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/db_organization", "michael", "michael");
     //postgresql-vertical-96442 -> database url
 
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-23-22-191-232.compute-1.amazonaws.com:5432/dftis94dvivs4i?sslmode=require", "sdlmyvygvzlogv", "413cbeafac2c5a90adf9a6c2072da44c2c576de51dd492323dff7c6b382b5737");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-23-22-191-232.compute-1.amazonaws.com:5432/dftis94dvivs4i?ssl=true", "sdlmyvygvzlogv", "413cbeafac2c5a90adf9a6c2072da44c2c576de51dd492323dff7c6b382b5737");
 }
